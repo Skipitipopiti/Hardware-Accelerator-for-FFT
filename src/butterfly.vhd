baaeaa392ab7315aa1934403cdd69546853118a9
Marco Spataro <marcospataro03@gmail.com>
2025-12-18 19:41:49 +0100

    Scheletro butterfly

--- src/butterfly.vhd
+++ src/butterfly.vhd
@@ -9,40 +9,37 @@ use ieee.fixed_float_types.all;
 entity butterfly is
     generic ( N : natural := 24 );
     port (
-        clk     : in  std_logic;
-        arst    : in  std_logic;
-        start   : in  std_logic;
-        done    : out std_logic;
-        A       : in  sfixed(0 downto (1 - N));
-        B       : in  sfixed(0 downto (1 - N));
-        Wr      : in  sfixed(0 downto (1 - N));
-        Wi      : in  sfixed(0 downto (1 - N));
-        Ap      : out sfixed(0 downto (1 - N));
-        Bp      : out sfixed(0 downto (1 - N))
+        clk      : in  std_logic;
+        arst     : in  std_logic;
+        start    : in  std_logic;
+        done     : out std_logic;
+        SF_2H_1L : in  std_logic; -- fattore di scala
+        A        : in  sfixed(0 downto 1-N);
+        B        : in  sfixed(0 downto 1-N);
+        Wr       : in  sfixed(0 downto 1-N);
+        Wi       : in  sfixed(0 downto 1-N);
+        Ap       : out sfixed(0 downto 1-N);
+        Bp       : out sfixed(0 downto 1-N)
     );
 end entity butterfly;
 
 -- Implementazione comportamentale con macchina a stati finiti
 architecture Behavioral of butterfly is
-    type step_t is (IDLE, S1, S2, S3, S4, S5, S6, FINISH);
+    type step_t is (IDLE, S1, S2, S3, S4, S5, S6);
     type state_t is record
         step : step_t;
 
-        -- indica il primo ciclo di calcolo dopo lo start
-        --   (serve per la modalità continua)
-        first_cycle    : std_logic;
-
         -- finish_cycle: flag per capire se manca ancora metà ciclo di calcolo
-        --   prima di andare al done
+        -- prima di andare al done
+        -- 0 = manca metà ciclo
+        -- 1 = ciclo completo (fine modalità continua)
         finish_cycle : std_logic;
     end record;
 
     signal current_state, next_state : state_t;
 
-    type rf_t is array (0 to 5) of sfixed(0 downto (1 - N));
-    signal rf_in  : rf_t;
-    signal rf_out : rf_t;
-    signal rf_en  : std_logic_vector(0 to 5);
+    signal rf_en  : std_logic_vector(0 to 3);
+    
 begin
     process(clk, arst)
     begin
@@ -73,6 +70,8 @@ begin
 
         case current_state.step is
             when IDLE =>
+                next_state.finish_cycle <= '0';
+
                 if start = '1' then
                     next_state.step <= S1;
                 end if;
@@ -87,22 +86,49 @@ begin
             when S5 =>
                 next_state.step <= S6;
             when S6 =>
-                next_state.finish_cycle <= not start;
-                next_state.first_cycle <= '0';
-
                 if current_state.finish_cycle = '1' then
-                    next_state.step <= FINISH;
+                    next_state.step <= IDLE;
                 else
                     next_state.step <= S1;
                 end if;
-            when FINISH =>
-                if start = '0' then
-                    next_state.step <= IDLE;
-                    next_state.first_cycle <= '1';
-                end if;
-            when others =>
-                next_state <= IDLE;
+
+                next_state.finish_cycle <= not start;
         end case;
     end process;
 
+    -- Operazioni in ogni stato
+    process(current_state, A, B, Wr, Wi)
+    begin
+        -- Default
+        rf_en  <= (others => '0');
+        done   <= '0';
+        Ap     <= (others => '0');
+        Bp     <= (others => '0');
+
+        case current_state.step is
+            when IDLE =>
+                null;
+
+            when S1 =>
+                null;
+
+            when S2 =>
+                null;
+
+            when S3 =>
+                null;
+
+            when S4 =>
+                null;
+
+            when S5 =>
+                null;
+
+            when S6 =>
+                null;
+
+            when others =>
+                null;
+        end case;
+    end
 end
---
+++ src/butterfly_dp.vhd
@@ -0,0 +1,111 @@
+-- Datapath Butterfly
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.fixed_pkg.all;
+use ieee.fixed_float_types.all;
+
+entity butterfly_dp is
+    -- N: parallelismo dati in ingresso e in uscita
+    generic ( N : natural := 24 );
+    port (
+        clk      : in  std_logic;
+        arst     : in  std_logic;
+        rf_en    : in  std_logic_vector(0 to 3);
+        r_sum_en : in  std_logic;
+        r_ar     : in std_logic
+        r_ais    : in  std_logic;
+        sum_sel  : in  std_logic;  -- '0' per somma, '1' per sottrazione
+        A        : in  sfixed(0 downto 1-N);
+        B        : in  sfixed(0 downto 1-N);
+        Wr       : in  sfixed(0 downto 1-N);
+        Wi       : in  sfixed(0 downto 1-N);
+        Ap       : out sfixed(0 downto 1-N);
+        Bp       : out sfixed(0 downto 1-N)
+    );
+end entity butterfly_dp;
+
+-- TODO: SISTEMARE PARALLELISMO DATI E REGISTRI
+architecture Behavioral of butterfly_dp is
+    -- Il register file deve contenere valori con il parallelismo interno massimo
+    type rf_t is array (0 to 3) of sfixed;
+    signal rf_in  : rf_t;
+    signal rf_out : rf_t;
+
+    -- R_sum: sum register
+    signal r_sum_in  : sfixed(1 downto 1-N);
+    signal r_sum_out : sfixed(1 downto 1-N);
+
+    -- R_Ar: Ar register
+    signal r_ar_in  : sfixed(1 downto 1-N);
+    signal r_ar_out : sfixed(1 downto 1-N);
+
+    -- R_Ai: Ai register
+    signal r_ai_in  : sfixed(1 downto 1-N);
+    signal r_ai_out : sfixed(1 downto 1-N);
+
+    -- TODO: rivedere il parallelismo
+    signal mul_in1, mul_in2 : sfixed(0 downto 1-N);
+    signal mul_out : sfixed(1 downto 2-2*N);
+    signal mul_shift_out : sfixed(1 downto 1-N);
+
+    -- TODO: rivedere il parallelismo
+    signal sum_in1, sum_in1 : sfixed(0 downto 1-N);
+    signal add_out, sub_out, sum_out : sfixed(1 downto 1-N);
+    signal sum_out_ext : sfixed();
+
+begin
+    process(clk, arst)
+    begin
+        if arst = '1' then
+            rf_out <= (others => (others => '0'));
+            rs_out <= (others => '0');
+        elsif rising_edge(clk) then
+            -- Scrittura nel register file
+            for i in 0 to rf_en'length - 1 loop
+                if rf_en(i) = '1' then
+                    rf_out(i) <= rf_in(i);
+                end if;
+            end loop;
+
+            -- Scrittura in Rs
+            if rs_en = '1' then
+                rs_out <= rs_in;
+            end if;
+        end if;
+    end process;
+
+    MUL_INST: entity work.Multiplier
+        generic map ( N => N )
+        port map (
+            clk  => clk,
+            A    => mul_in1,
+            B    => mul_in2,
+            PROD => mul_out,
+            A2   => mult_shift_out
+        );
+
+    -- TODO: rivedere il parallelismo
+    ADD_INST: entity work.Adder
+        generic map ( N => N )
+        port map (
+            clk => clk,
+            A   => sum_in1,
+            B   => sum_in2,
+            SUM => add_out
+        );
+
+    -- TODO: rivedere il parallelismo
+    SUB_INST: entity work.Subtractor
+        generic map ( N => N )
+        port map (
+            clk => clk,
+            A   => sum_in1,
+            B   => sum_in2,
+            SUM => sub_out
+        );
+
+    sum_out <= add_out when sum_sel = '0' else sub_out;
+    sum_out_ext <= resize(sum_out, sum_out_ext'high, sum_out_ext'low);
+    rs_in <= sum_out;
+end architecture Behavioral;
