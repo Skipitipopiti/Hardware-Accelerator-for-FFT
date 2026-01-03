4b8f6a41fdbbe57bcbfbc8c28720ba4d2e196c50
Marco Spataro <marcospataro03@gmail.com>
2026-01-03 19:42:55 +0100

    Reso ROM rounder più ergonomico

--- src/operators.vhd
+++ src/operators.vhd
@@ -121,24 +121,44 @@ use ieee.fixed_pkg.all;
 use ieee.fixed_float_types.all;
 
 entity ROM_Rounder is 
-    -- n : # bit interi, m : # bit frazionari
-    generic ( n : positive; m : natural range 2 to natural'high );
+    generic (
+        -- i: numero di bit interi
+        i : positive;
+        -- f: numero di bit frazionari
+        f : natural;
+
+        -- Indice LSB post-arrotondamento
+        lsb : integer;
+
+        -- n: bit soggetti a rounding prima del LSB (incluso)
+        n : positive;
+        -- m: bit soggetti a rounding dopo il LSB (escluso)
+        m : positive
+    );
     port (
         cs       : in  std_logic;
-        addr     : in  ufixed(n - 1 downto -m);
-        data_out : out unsigned(n - 1 downto 0)
+        data_in  : in  sfixed(i - 1 downto -f);
+        data_out : out sfixed(i - 1 downto lsb)
     );
 end entity ROM_Rounder;
 
 architecture Behavioral of ROM_Rounder is
-    signal integer_part : unsigned(n - 1 downto 0);
+    -- Parti intera e frazionaria ai fini dell'arrotondamento
+    -- ossia "spostando" la virgola in modo che il LSB diventi il bit meno significativo
+    signal integer_part    : unsigned(n - 1 downto 0);
     signal fractional_part : unsigned(m - 1 downto 0);
-begin
-    integer_part <= unsigned(addr(n - 1 downto 0));
-    fractional_part <= unsigned(to_slv(addr(-1 downto -m)));
 
-    process(cs, integer_part, fractional_part)
-        variable N_Temp    : unsigned(n - 1 downto 0); -- Valore arrotondato in uscita
+begin
+    -- Strettamente minore per non perdere il bit di segno
+    assert (lsb + n <   i)  report "Errore: n + lsb deve essere < di i" severity failure;
+    assert (lsb - m >= -f) report "Errore: lsb - m deve essere >= di -f" severity failure;
+
+    integer_part <= unsigned(data_in(lsb + n - 1 downto lsb));
+    fractional_part <= unsigned(to_slv(data_in(lsb - 1 downto lsb - m)));
+
+    process(cs, data_in, integer_part, fractional_part)
+        -- Valore arrotondato in uscita
+        variable N_Temp    : unsigned(n - 1 downto 0);
         -- flag che mi dice se arrotondare per eccesso o meno
         variable round_up  : boolean;
 
@@ -150,7 +170,7 @@ begin
             if fractional_part(m - 1) = '0' then
                 round_up := false;
 
-            elsif (to_integer(unsigned(fractional_part(m - 2 downto 0))) /= 0) then
+            elsif to_integer(unsigned(fractional_part(m - 2 downto 0))) /= 0 then
                 -- > 0.5   -> arrotonda x eccesso !!!!! m>1 xk se m=1 non esistono altri bit che devo controllare
                 round_up := true;
             else
@@ -171,7 +191,8 @@ begin
             N_Temp := integer_part;
         end if;
         
-        data_out <= N_Temp;
+        data_out(i - 1 downto lsb + n)   <= data_in(i - 1 downto lsb + n);
+        data_out(lsb + n - 1 downto lsb) <= to_sfixed(std_logic_vector(N_Temp), lsb + n - 1, lsb);
     end process;
     
 end Behavioral;
--- tb/tb_rom_rounder.vhd
+++ tb/tb_rom_rounder.vhd
@@ -11,18 +11,27 @@ architecture tb of tb_ROM_Rounder is
     constant n : natural := 3;
     constant m : natural := 5;
 
+    constant i : natural := n + 1;
+    constant f : natural := m + 3;
+
     signal cs       : std_logic;
-    signal addr     : ufixed(n - 1 downto -m);
-    signal data_out : unsigned(n - 1 downto 0);
+    signal data_in  : sfixed(i - 1 downto -f);
+    signal data_out : sfixed(i - 1 downto 0);
 
     constant TbPeriod : time := 10 ns;
 begin
 
     dut : entity work.ROM_Rounder
-    generic map ( n => n, m => m )
-    port map (cs       => cs,
-              addr     => addr,
-              data_out => data_out);
+    generic map (
+        i => n+1, f => f,
+        lsb => 0,
+        n => n, m => m
+    )
+    port map (
+        cs => cs,
+        data_in  => data_in,
+        data_out => data_out
+    );
 
     stimuli : process
     begin
@@ -30,13 +39,13 @@ begin
 
         wait for TbPeriod/2 + 1 ns;
 
-        addr <= to_ufixed(0, addr'high, addr'low),
-            to_ufixed(0.7, addr'high, addr'low)  after 1*TbPeriod, -- round up 
-            to_ufixed(3.2, addr'high, addr'low)  after 2*TbPeriod, -- round down
-            to_ufixed(7.6, addr'high, addr'low)  after 3*TbPeriod, -- saturazione
-            to_ufixed(1.5, addr'high, addr'low)  after 4*TbPeriod, -- nearest even
-            to_ufixed(4.5, addr'high, addr'low)  after 5*TbPeriod, -- nearest even
-            to_ufixed(4.1, addr'high, addr'low)  after 6*TbPeriod; -- test CS
+        data_in <= to_sfixed(0, data_in'high, data_in'low),
+            to_sfixed(0.7, data_in'high, data_in'low)  after 1*TbPeriod, -- round up 
+            to_sfixed(3.2, data_in'high, data_in'low)  after 2*TbPeriod, -- round down
+            to_sfixed(7.6, data_in'high, data_in'low)  after 3*TbPeriod, -- saturazione
+            to_sfixed(1.5, data_in'high, data_in'low)  after 4*TbPeriod, -- nearest even
+            to_sfixed(4.5, data_in'high, data_in'low)  after 5*TbPeriod, -- nearest even
+            to_sfixed(4.1, data_in'high, data_in'low)  after 6*TbPeriod; -- test CS
         
         wait;
     end process;
